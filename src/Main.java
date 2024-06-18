5841035bbb84e740e9f24850f66c3bf79ff55b84
Fola Osibo <osibofolawewo@gmail.com>
2024-06-18 03:23:09 +0100

    Search Engine v5

--- src/Main.java
+++ src/Main.java
@@ -32,58 +32,58 @@ public class Main {
             }
         }
 
-        ArrayList<String[]> separatedRelevantDocContents =
-                new ArrayList<>();
-        for(String relevantDocContent : relevantDocContents) {
-            separatedRelevantDocContents.add(relevantDocContent.split(" "));
-        }
-
-        ArrayList<ArrayList<String>> filteredRelevantDocContents =
-                new ArrayList<>();
-        for(String[] separatedRelevantDocContent : separatedRelevantDocContents) {
-            ArrayList<String> preFilteredRelevantDocContents =
+        if(!query.contains(" ")) {
+            ArrayList<String[]> separatedRelevantDocContents =
                     new ArrayList<>();
-            for(String separatedRelevantDocContentWord : separatedRelevantDocContent){
-                if(separatedRelevantDocContentWord.toUpperCase().contains(query.toUpperCase())){
-                    preFilteredRelevantDocContents.add(separatedRelevantDocContentWord);
-                }
+            for (String relevantDocContent : relevantDocContents) {
+                separatedRelevantDocContents.add(relevantDocContent.split(" "));
             }
-            filteredRelevantDocContents.add(preFilteredRelevantDocContents);
-        }
 
-        ArrayList<String> associatedRelevantDocNames = new ArrayList<>();
-        for(ArrayList<String> filteredRelevantDocContent :
-                filteredRelevantDocContents) {
-            int i = 0;
-            int k = 0;
-            for(String docName : documents.keySet()){
-                for(String relevantDocContent : filteredRelevantDocContent){
-                    if(documents.get(docName).toUpperCase().contains(relevantDocContent.toUpperCase())){
-                        k++;
+            ArrayList<ArrayList<String>> filteredRelevantDocContents =
+                    new ArrayList<>();
+            for (String[] separatedRelevantDocContent : separatedRelevantDocContents) {
+                ArrayList<String> preFilteredRelevantDocContents =
+                        new ArrayList<>();
+                for (String separatedRelevantDocContentWord : separatedRelevantDocContent) {
+                    if (separatedRelevantDocContentWord.toUpperCase().contains(query.toUpperCase())) {
+                        preFilteredRelevantDocContents.add(separatedRelevantDocContentWord);
                     }
                 }
-                if (k > i){
-                    i = k;
-                    associatedRelevantDocNames.add(docName);
+                filteredRelevantDocContents.add(preFilteredRelevantDocContents);
+            }
+
+            ArrayList<String> associatedRelevantDocNames = new ArrayList<>();
+            for (ArrayList<String> filteredRelevantDocContent :
+                    filteredRelevantDocContents) {
+                int i = 0;
+                int k = 0;
+                for (String docName : documents.keySet()) {
+                    for (String relevantDocContent : filteredRelevantDocContent) {
+                        if (documents.get(docName).toUpperCase().contains(relevantDocContent.toUpperCase())) {
+                            k++;
+                        }
+                    }
+                    if (k > i) {
+                        i = k;
+                        associatedRelevantDocNames.add(docName);
+                    }
+                }
+            }
+
+            if (!(relevantDocNames.isEmpty())) {
+                System.out.println("Documents:");
+                for (String docName : relevantDocNames) {
+                    System.out.println("- " + docName);
+                }
+            }
+
+            if (!(filteredRelevantDocContents.isEmpty())) {
+                System.out.println("Contents:");
+                for (int i = 0; i < filteredRelevantDocContents.size(); i++) {
+                    System.out.println("- " + associatedRelevantDocNames.get(i) + " " + filteredRelevantDocContents.get(i));
                 }
             }
         }
-
-        if(!(relevantDocNames.isEmpty())){
-            System.out.println("Documents:");
-            for(String docName : relevantDocNames) {
-                System.out.println("- " + docName);
-            }
-        }
-
-        if(!(filteredRelevantDocContents.isEmpty())){
-            System.out.println("Contents:");
-//            for(int i = 0; i < filteredRelevantDocContents.size(); i++) {
-//                System.out.println("- " + associatedRelevantDocNames.get(i) + " " + filteredRelevantDocContents.get(i));
-//            }
-        }
-
-        System.out.println(relevantDocContents);
 
     }
 }
