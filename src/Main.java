fdcf1009584cc52dda33148f419a97e06d2c8ded
Fola Osibo <osibofolawewo@gmail.com>
2024-06-19 02:31:55 +0100

    Search Engine v6

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.ArrayList;
 
@@ -9,7 +10,7 @@ public class Main {
 
         documents.put("Doc1", "this is a");
         documents.put("Doc2", "this is a test");
-        documents.put("Doc3", "Doc");
+        documents.put("Doc3", "Doc a");
         documents.put("Doc4", "of a search engine");
 
         ArrayList<String> docNames = Utilities.getDocNames(documents);
@@ -29,6 +30,13 @@ public class Main {
         for(String docContent : docContents){
             if(docContent.toUpperCase().contains(query.toUpperCase())){
                 relevantDocContents.add(docContent);
+            }
+        }
+
+        if (!(relevantDocNames.isEmpty())) {
+            System.out.println("Documents:");
+            for (String docName : relevantDocNames) {
+                System.out.println("- " + docName);
             }
         }
 
@@ -70,19 +78,47 @@ public class Main {
                 }
             }
 
-            if (!(relevantDocNames.isEmpty())) {
-                System.out.println("Documents:");
-                for (String docName : relevantDocNames) {
-                    System.out.println("- " + docName);
-                }
-            }
-
             if (!(filteredRelevantDocContents.isEmpty())) {
                 System.out.println("Contents:");
                 for (int i = 0; i < filteredRelevantDocContents.size(); i++) {
                     System.out.println("- " + associatedRelevantDocNames.get(i) + " " + filteredRelevantDocContents.get(i));
                 }
             }
+        }
+
+        else{
+            int splitNumber = query.split(" ").length;
+            ArrayList<ArrayList<String>> filteredRelevantDocContents = new ArrayList<>();
+
+            for(String relevantDocContent : relevantDocContents){
+                ArrayList<String> preFilteredRelevantDocContents = new ArrayList<>();
+                for(String word : relevantDocContent.split(" ")){
+                    preFilteredRelevantDocContents.add(word);
+                }
+
+                int sizeOfPreFilteredRelevantDocContents = preFilteredRelevantDocContents.size();
+
+                ArrayList<String> finalPreFilteredRelevantDocContents = new ArrayList<>();
+
+                ArrayList<String> polishedPreFilteredRelevantDocContents = new ArrayList<>();
+
+                    for (int i = 0; i <= (sizeOfPreFilteredRelevantDocContents - splitNumber); i++) {
+                        String word = "";
+                        for (int j = 0; j < splitNumber; j++) {
+                            word += preFilteredRelevantDocContents.get(j) + " ";
+                        }
+                        finalPreFilteredRelevantDocContents.add(word);
+                        preFilteredRelevantDocContents.removeFirst();
+                    }
+
+                    for(String finalPreFilteredRelevantDocContent : finalPreFilteredRelevantDocContents){
+                        if(finalPreFilteredRelevantDocContent.toUpperCase().contains(query.toUpperCase())){
+                            polishedPreFilteredRelevantDocContents.add(finalPreFilteredRelevantDocContent);
+                        }
+                    }
+                    filteredRelevantDocContents.add(polishedPreFilteredRelevantDocContents);
+            }
+            System.out.println(filteredRelevantDocContents);
         }
 
     }
